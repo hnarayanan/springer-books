c7353f2ae0fa9ed98dc7721a26db82e98e88d345
Harish Narayanan <hnarayanan@gmail.com>
2020-06-13 14:28:17 +0100

    Update generator and commit generated output

--- generate.py
+++ generate.py
@@ -3,13 +3,21 @@ from jinja2 import Environment, FileSystemLoader
 
 
 books_df = pd.read_excel("./input/Free+English+textbooks.xlsx")
-grouped_books = books_df.groupby(["English Package Name", "Product Type"])
+grouped_books_df = books_df.groupby(["English Package Name"])
+
 
 loader = FileSystemLoader(searchpath="./templates/")
 env = Environment(loader=loader)
 template = env.get_template("index.html")
 
+grouped_books = {}
+
+for group, books in grouped_books_df:
+    grouped_books[group] = []
+    for book in books.iterrows():
+        grouped_books[group].append(book)
+
 rendered_template = template.render(grouped_books=grouped_books)
 
-with open("./output/index.html", "w") as f:
+with open("index.html", "w") as f:
     f.write(rendered_template)
