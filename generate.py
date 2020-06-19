c4f14317437dd0b32d35653d781c76413cbdf457
Harish Narayanan <hnarayanan@gmail.com>
2020-06-19 13:35:15 +0100

    Clean up Goodreads rating functionality

--- generate.py
+++ generate.py
@@ -3,6 +3,14 @@ import requests
 import configparser
 from jinja2 import Environment, FileSystemLoader
 
+
+def pluralize(number, singular="", plural="s"):
+    if number == 1:
+        return singular
+    else:
+        return plural
+
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 good_reads_api_key = config["goodreads.com"]["key"]
@@ -12,16 +20,24 @@ grouped_books_df = books_df.groupby(["English Package Name"])
 
 loader = FileSystemLoader(searchpath="./templates/")
 env = Environment(loader=loader)
+env.filters["pluralize"] = pluralize
 template = env.get_template("index.html")
 
 isbn_hyphen_maps = [{}, {}]
 isbn_review_map = {}
 
 for book in books_df.iterrows():
-    isbn_hyphen_maps[0][book[1]["Print ISBN"]] = book[1]["Print ISBN"].replace('-', '')
-    isbn_hyphen_maps[1][book[1]["Electronic ISBN"]] = book[1]["Electronic ISBN"].replace('-', '')
+    isbn_hyphen_maps[0][book[1]["Print ISBN"]] = book[1]["Print ISBN"].replace("-", "")
+    isbn_hyphen_maps[1][book[1]["Electronic ISBN"]] = book[1][
+        "Electronic ISBN"
+    ].replace("-", "")
 for map in isbn_hyphen_maps:
-    response = requests.get("https://www.goodreads.com/book/review_counts.json?isbns=" + ','.join(map.values()) + 'key=' + good_reads_api_key)
+    response = requests.get(
+        "https://www.goodreads.com/book/review_counts.json?isbns="
+        + ",".join(map.values())
+        + "key="
+        + good_reads_api_key
+    )
     for review in response.json()["books"]:
         isbn_review_map[review["isbn13"]] = review
 
@@ -30,7 +46,10 @@ grouped_books = {}
 for group, books in grouped_books_df:
     grouped_books[group] = []
     for book in books.iterrows():
-        book[1]["goodreads_review"] = isbn_review_map.get(isbn_hyphen_maps[0][book[1]["Print ISBN"]], isbn_review_map.get(isbn_hyphen_maps[1][book[1]["Electronic ISBN"]], None))
+        book[1]["goodreads_review"] = isbn_review_map.get(
+            isbn_hyphen_maps[0][book[1]["Print ISBN"]],
+            isbn_review_map.get(isbn_hyphen_maps[1][book[1]["Electronic ISBN"]], None),
+        )
         grouped_books[group].append(book)
 
 rendered_template = template.render(grouped_books=grouped_books)
