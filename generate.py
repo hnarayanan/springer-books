90e63d2f6579d5b19d0191c9cf0298cca7f496a3
Kevin Le <kle622@gmail.com>
2020-06-18 21:32:25 -0700

    Try to find e-isbn if available

--- generate.py
+++ generate.py
@@ -11,20 +11,23 @@ loader = FileSystemLoader(searchpath="./templates/")
 env = Environment(loader=loader)
 template = env.get_template("index.html")
 
-isbn_hyphen_map = {}
+isbn_hyphen_maps = [{}, {}]
 isbn_review_map = {}
+
 for book in books_df.iterrows():
-    isbn_hyphen_map[book[1]["Print ISBN"]] = book[1]["Print ISBN"].replace('-', '')
-response = requests.get("https://www.goodreads.com/book/review_counts.json?isbns=" + ','.join(isbn_hyphen_map.values()) + 'key=' + good_reads_api_key)
-for review in response.json()["books"]:
-    isbn_review_map[review["isbn13"]] = review
+    isbn_hyphen_maps[0][book[1]["Print ISBN"]] = book[1]["Print ISBN"].replace('-', '')
+    isbn_hyphen_maps[1][book[1]["Electronic ISBN"]] = book[1]["Electronic ISBN"].replace('-', '')
+for map in isbn_hyphen_maps:
+    response = requests.get("https://www.goodreads.com/book/review_counts.json?isbns=" + ','.join(map.values()) + 'key=' + good_reads_api_key)
+    for review in response.json()["books"]:
+        isbn_review_map[review["isbn13"]] = review
 
 grouped_books = {}
 
 for group, books in grouped_books_df:
     grouped_books[group] = []
     for book in books.iterrows():
-        book[1]["goodreads_review"] = isbn_review_map.get(isbn_hyphen_map[book[1]["Print ISBN"]], None)
+        book[1]["goodreads_review"] = isbn_review_map.get(isbn_hyphen_maps[0][book[1]["Print ISBN"]], isbn_review_map.get(isbn_hyphen_maps[1][book[1]["Electronic ISBN"]], None))
         grouped_books[group].append(book)
 
 rendered_template = template.render(grouped_books=grouped_books)
